5e331d460dcaa57a7daa8e61b469f240eac59bce
Softwarica College Lab <softwarica@softwarica.com>
2019-04-07 11:28:03 +0545

    Initial commit

---
+++ app/src/main/java/com/e/bmisuresh/MainActivity.java
@@ -0,0 +1,17 @@
+package com.e.bmisuresh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bBmiCalculate;
+    private EditText eTHeight, etWeight,eTBM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
